004437669eb484c92f3a741120067168191837a3
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-11-19 11:32:49 +0100

    footer styling

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
-  return <div>Instagreen 2024</div>;
+  return <div className="footer-main">Instagreen 2024</div>;
 }
 
 export default Footer;
