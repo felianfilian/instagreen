faaba23d952f4270cf2592a0dbbeb615a8be3eb9
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-11-23 11:49:24 +0100

    icon bar update

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -9,6 +9,8 @@ function Post(props) {
       <div className="icon-bar">
         <img src={require("./../img/icons/comment.png")} alt="Comment"></img>
         <img src={require("./../img/icons/heart.png")} alt="Like"></img>
+        <img src={require("./../img/icons/star.png")} alt="Favorite"></img>
+        <img src={require("./../img/icons/send.png")} alt="Share"></img>
       </div>
     </div>
   );
