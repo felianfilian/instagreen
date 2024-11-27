9a577f37407d1e941775390ad38a5d77a8da69f0
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-11-27 13:26:50 +0100

    icon evvent

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./Post.css";
 
 function Post(props) {
+  const toogleImage = () => {
+    alert("toggle");
+  };
+
   return (
     <div className="post">
       <p className="post-title">{props.title}</p>
       <p className="post-text">{props.text}</p>
       <div className="icon-bar">
-        <img src={require("./../img/icons/comment.png")} alt="Comment"></img>
+        <img
+          onClick={() => toogleImage()}
+          src={require("./../img/icons/comment.png")}
+          alt="Comment"
+        ></img>
         <img src={require("./../img/icons/heart.png")} alt="Like"></img>
         <img src={require("./../img/icons/star.png")} alt="Favorite"></img>
         <img src={require("./../img/icons/send.png")} alt="Share"></img>
